1aeae06aa69e934eeec75bd9549c50c59fe29cb8
Jeremias Acurso <jeremias.acurso@hotmail.com>
2023-03-23 11:05:21 -0300

    nuevo database model llamado Post con titulo autor y cuerpo

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.urls import (
+    reverse,
+)  # nos permite referenciarnos a un objeto por su URL template name.
 
 # Create your models here.
+
+
+class Post(models.Model):
+    """creamos una subclase de models.Model 'Post'"""
+
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE)
+    body = models.TextField()
+
+    def __str__(self):  # para poder ponerle un titulo al djangoshell/admin
+        return self.title
+
+    def get_absolute_url(self):  # usa el url nombrado post_detail y lo pasa al pk
+        return reverse("post_detail", kwargs={"pk": self.pk})
