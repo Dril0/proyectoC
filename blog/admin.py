c6bc1c338d87499617380f9e8da28edc5a9c2068
Jeremias Acurso <jeremias.acurso@hotmail.com>
2023-03-23 21:14:22 -0300

    registramos el models Post para poder verlo en el admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post  # importamos los modelos que queremos ver en admin.
 
 # Register your models here.
+
+admin.site.register(
+    Post
+)  # registramos el modelo Post al admin. ahora lo podemos ver ahi.
