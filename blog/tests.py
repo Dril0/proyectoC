8e292c6badaad6e107e85db484754ff26f8917e2
Jeremias Acurso <jeremias.acurso@hotmail.com>
2023-03-25 10:58:41 -0300

    importamos reverse y creamos test nuevos, detalles en los comentarios

--- blog/tests.py
+++ blog/tests.py
@@ -1,6 +1,7 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model  # nos permite referirnos a nuestro user.
 from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
 
@@ -22,3 +23,33 @@ class BlogTests(TestCase):
         self.assertEqual(self.post.author.username, "testuser")
         self.assertEqual(str(self.post), "proyectoC")
         self.assertEqual(self.post.get_absolute_url(), "/post/1/")
+
+    def test_url_exist_at_correct_location_listview(self):  # verifica que exista home
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_exist_at_correct_location_detailview(
+        self,
+    ):  # verifica que exista post_detail
+        response = self.client.get("/post/1/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_listview(self):
+        response = self.client.get(reverse("home"))  # verifica que use el url home
+        self.assertEqual(response.status_code, 200)  # retorne un 200 status code
+        self.assertContains(
+            response, "el mejor proyecto"
+        )  # coincide el contenido con el que creamos en SetUpTestData
+        self.assertTemplateUsed(
+            response, "home.html"
+        )  # utiliza el home.html correctamente
+
+    def test_post_detailview(self):
+        response = self.client.get(reverse("post_detail", kwargs={"pk": self.post.pk}))
+        no_response = self.client.get(
+            "/post/10000/"
+        )  # no necesitamos un response al post numero 10000 porque no tenemos tantos, y evitamos el 404 http.
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(no_response.status_code, 404)
+        self.assertContains(response, "proyectoC")
+        self.assertTemplateUsed(response, "post_detail.html")
