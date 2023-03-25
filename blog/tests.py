6765b2bdd9b79afcd32843a57a0653ae9826c8b7
Jeremias Acurso <jeremias.acurso@hotmail.com>
2023-03-25 00:25:40 -0300

    test para testear el usuario y los campos del Post

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model  # nos permite referirnos a nuestro user.
+from .models import Post
 
 # Create your tests here.
+
+
+class BlogTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = get_user_model().objects.create_user(
+            username="testuser", email="test@email.com", password="secret"
+        )
+
+        cls.post = Post.objects.create(
+            title="proyectoC", body="el mejor proyecto", author=cls.user
+        )
+
+    def test_post_model(self):
+        self.assertEqual(self.post.title, "proyectoC")
+        self.assertEqual(self.post.body, "el mejor proyecto")
+        self.assertEqual(self.post.author.username, "testuser")
+        self.assertEqual(str(self.post), "proyectoC")
+        self.assertEqual(self.post.get_absolute_url(), "/post/1/")
