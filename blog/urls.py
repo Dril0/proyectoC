c8991a0791116091d5c6e57003052cc49ce20778
Jeremias Acurso <jeremias.acurso@hotmail.com>
2023-03-24 23:54:50 -0300

    creamos la ruta a los post por el nuimero de pk

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import BlogListView  # importamos las funciones que quermos utilizar
+from .views import (
+    BlogListView,
+    BlogDetailView,
+)  # importamos las funciones que quermos utilizar
 
 urlpatterns = [
     path(
+        "post/<int:pk>", BlogDetailView.as_view(), name="post_detail"
+    ),  # pk es el id que agrega django a la database, es un entero por eso <int:pk>
+    path(
         "", BlogListView.as_view(), name="home"
-    )  # creamos una ruta vacia a view, con el name home
+    ),  # creamos una ruta vacia a view, con el name home
 ]
